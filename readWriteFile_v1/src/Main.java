834e2551676d2ced64bfbfc46fff450e9c9cde69
Stepanova Tatyana <stepanova.t.qa@gmail.com>
2024-05-16 12:24:57 +0300

    update main.class

--- readWriteFile_v1/src/Main.java
+++ readWriteFile_v1/src/Main.java
@@ -5,33 +5,27 @@ public class Main {
         String readFile = "text.txt";
         String writeFile = "textNew.txt";
 
-        try(FileReader fileReader = new FileReader(readFile);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            FileWriter fileWriter = new FileWriter(writeFile);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter))
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(readFile));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(writeFile)))
         {
             String line; //считываемая строка
             int vowelCount = 0; //гласные
             int consonantCount = 0; //согласные
 
             while ((line = bufferedReader.readLine()) != null) {
-                for (char symbol: line.toCharArray()) { //перебор посимвольно считываемой строки
-                    if (Character.isLetter(symbol)) { //если символ - буква Unicode
-                        if (isVowel(symbol)) { //если буква гласная - заменить и посчитать кол-во
-                            bufferedWriter.write('a');
-                            vowelCount++;
-                        } else {
-                            bufferedWriter.write('м');
-                            consonantCount++;
-                        }
-                    } else {
-                        bufferedWriter.write(symbol); //запись символа (не буква)
-                    }
-                }
 
+                String replacedLine = line.toLowerCase().replaceAll("[аеёийоуыэюя]", "a")
+                        .replaceAll("[бвгджзклмнпрстфхцчшщ]", "м");
+
+                //количествo гласных и согласных букв (кроме Ь и Ъ, они не относятся к гласным или согласным)
+                vowelCount += line.toLowerCase().replaceAll("[^аеёиоуыэюя]", "").length();
+                consonantCount += line.toLowerCase().replaceAll("[^бвгджзйклмнпрстфхцчшщ]", "").length();
+
+                bufferedWriter.write(replacedLine);
                 bufferedWriter.newLine();
             }
 
+            System.out.println("В файле успешно произведена замена гласных букв на 'а', согласных на 'м'. За исключение Ь и Ъ знаков.");
             System.out.println("Количество гласных букв в файле: " + vowelCount);
             System.out.println("Количество согласных букв в файле: " + consonantCount);
 
@@ -41,9 +35,4 @@ public class Main {
 
     }
 
-    //перевод букв в нижний регистр и проверка - гласная ли буква
-    private static boolean isVowel(char letter) {
-        letter = Character.toLowerCase(letter);
-        return letter == 'а' || letter == 'е' || letter == 'ё' || letter == 'и' || letter == 'о' || letter == 'у' || letter == 'ы' || letter == 'э' || letter == 'ю' || letter == 'я';
-    }
 }
